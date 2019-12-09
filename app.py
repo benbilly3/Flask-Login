9cc89a5e488811457ee857a39cd06ab21d279d96
benfillygaming888 <benfillygaming888@benfillygaming888deMacBook-Pro.local>
2019-12-09 18:55:38 +0800

    google login

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from myproject import app,db
-from flask import render_template, redirect, request, url_for, flash,abort
+from flask import render_template, redirect, request, url_for, flash,Flask, redirect, url_for, render_template, session
 from flask_login import login_user,login_required,logout_user
 from myproject.models import User
 from myproject.forms import LoginForm, RegistrationForm
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_dance.contrib.google import make_google_blueprint, google
+from flask_login import logout_user
+import os
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = '1'
+os.environ["OAUTHLIB_RELAX_TOKEN_SCOPE"] = '1'
+#######################################################################
+#######################################################################
 
-
+#Normal_Member_Login
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -15,6 +22,7 @@ def home():
 @login_required
 def welcome_user():
     return render_template('welcome_user.html')
+
 
 @app.route('/logout')
 @login_required
@@ -26,18 +34,17 @@ def logout():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-
     form = LoginForm()
     if form.validate_on_submit():
         # Grab the user from our User Models table
         user = User.query.filter_by(email=form.email.data).first()
-        
+
         # Check that the user was supplied and the password is right
         # The verify_password method comes from the User object
         # https://stackoverflow.com/questions/2209755/python-operation-vs-is-not
 
         if user.check_password(form.password.data) and user is not None:
-            #Log in the user
+            # Log in the user
 
             login_user(user)
             flash('Logged in successfully.')
@@ -48,11 +55,12 @@ def login():
 
             # So let's now check if that next exists, otherwise we'll go to
             # the welcome page.
-            if next == None or not next[0]=='/':
+            if next == None or not next[0] == '/':
                 next = url_for('welcome_user')
 
             return redirect(next)
     return render_template('login.html', form=form)
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -68,6 +76,55 @@ def register():
         flash('Thanks for registering! Now you can login!')
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
+
+#Google_Login
+blueprint = make_google_blueprint(
+    client_id="750796013081-i2sr7euqejkpk5e5bcppnbol6g1dpus3.apps.googleusercontent.com",#"######.apps.googleusercontent.com",
+    client_secret="TeCTIoCgSQWOx_iVsQZjV8pq",
+    # reprompt_consent=True,
+    offline=True,
+    scope=["profile", "email"]
+)
+
+@app.route('/welcome_google')
+def welcome_google():
+    resp = google.get("/oauth2/v2/userinfo")
+    assert resp.ok, resp.text
+    email=resp.json()["email"]
+    return render_template("welcome_google.html",email=email)
+
+app.register_blueprint(blueprint, url_prefix="/login")
+@app.route("/login/google")
+def google_login():
+    if not google.authorized:
+        return render_template(url_for("google.login"))
+
+    resp = google.get("/oauth2/v2/userinfo")
+    assert resp.ok, resp.text
+    email=resp.json()["email"]
+    return render_template("welcome_google.html",email=email)
+
+from flask import Flask, redirect
+from flask_dance.contrib.google import make_google_blueprint, google
+from flask_login import logout_user
+
+
+# blueprint = make_google_blueprint()
+# app.register_blueprint(blueprint, url_prefix="/login")
+
+@app.route("/login/logout")
+def google_logout():
+    token = blueprint.token["access_token"]
+    resp = google.post(
+        "https://accounts.google.com/o/oauth2/revoke",
+        params={"token": token},
+        headers={"Content-Type": "application/x-www-form-urlencoded"}
+    )
+    assert resp.ok, resp.text
+    logout_user()        # Delete Flask-Login's session cookie
+    del blueprint.token  # Delete OAuth token from storage
+    return redirect(url_for('welcome_user'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
