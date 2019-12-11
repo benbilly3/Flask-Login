b026f4b6489d0c1c533bca33aa5fac08d5072264
benfillygaming888 <benfillygaming888@benfillygaming888deMacBook-Pro.local>
2019-12-11 10:28:47 +0800

    finish google OAuth,Start FB

--- app.py
+++ app.py
@@ -1,36 +1,25 @@
 from myproject import app,db
-from flask import render_template, redirect, request, url_for, flash,Flask, redirect, url_for, render_template, session
+from flask import request,flash,Flask, redirect, url_for, render_template
 from flask_login import login_user,login_required,logout_user
 from myproject.models import User
-from myproject.forms import LoginForm, RegistrationForm
-from werkzeug.security import generate_password_hash, check_password_hash
+from myproject.forms import LoginForm,RegistrationForm
+from config import google_client_id,google_client_secret,fb_client_id,fb_client_secret
 from flask_dance.contrib.google import make_google_blueprint, google
-from flask_login import logout_user
+from flask_dance.contrib.facebook import make_facebook_blueprint,facebook
 import os
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = '1'
 os.environ["OAUTHLIB_RELAX_TOKEN_SCOPE"] = '1'
-#######################################################################
-#######################################################################
 
-#Normal_Member_Login
+
+########Normal_Member_Login########
 @app.route('/')
 def home():
     return render_template('home.html')
-
 
 @app.route('/welcome')
 @login_required
 def welcome_user():
     return render_template('welcome_user.html')
-
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    flash('You logged out!')
-    return redirect(url_for('home'))
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -77,54 +66,93 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
-#Google_Login
-blueprint = make_google_blueprint(
-    client_id="750796013081-i2sr7euqejkpk5e5bcppnbol6g1dpus3.apps.googleusercontent.com",#"######.apps.googleusercontent.com",
-    client_secret="TeCTIoCgSQWOx_iVsQZjV8pq",
+########Google_Login########
+
+google_Oauth = make_google_blueprint(
+    client_id=google_client_id,
+    client_secret=google_client_secret,
     # reprompt_consent=True,
     offline=True,
-    scope=["profile", "email"]
+    scope=["profile", "email"],
+    redirect_url='/welcome_google'
 )
+
+app.register_blueprint(google_Oauth, url_prefix="/login")
+@app.route("/login/google")
+def google_login():
+    if not google.authorized:
+        return render_template(url_for("google.login"))
+    return render_template("welcome_google.html",email=email)
 
 @app.route('/welcome_google')
 def welcome_google():
     resp = google.get("/oauth2/v2/userinfo")
     assert resp.ok, resp.text
     email=resp.json()["email"]
-    return render_template("welcome_google.html",email=email)
+    # check gmail in db,if not in db,create user data
+    user = User.query.filter_by(email=email).first()
+    if user is None:
+        user = User(
+            email=email,
+            username=email,
+            password='123456'
+        )
 
-app.register_blueprint(blueprint, url_prefix="/login")
-@app.route("/login/google")
-def google_login():
-    if not google.authorized:
-        return render_template(url_for("google.login"))
+        db.session.add(user)
+        db.session.commit()
+        user = User.query.filter_by(email=email).first()
 
-    resp = google.get("/oauth2/v2/userinfo")
-    assert resp.ok, resp.text
-    email=resp.json()["email"]
-    return render_template("welcome_google.html",email=email)
+    # and log in
+    login_user(user)
 
-from flask import Flask, redirect
-from flask_dance.contrib.google import make_google_blueprint, google
-from flask_login import logout_user
+    # If a user was trying to visit a page that requires a login
+    # flask saves that URL as 'next'.
+    next = request.args.get('next')
 
+    # So let's now check if that next exists, otherwise we'll go to
+    # the welcome page.
+    if next == None or not next[0] == '/':
+        next = url_for('welcome_user')
+        return redirect(next)
 
-# blueprint = make_google_blueprint()
-# app.register_blueprint(blueprint, url_prefix="/login")
-
-@app.route("/login/logout")
-def google_logout():
-    token = blueprint.token["access_token"]
-    resp = google.post(
-        "https://accounts.google.com/o/oauth2/revoke",
-        params={"token": token},
-        headers={"Content-Type": "application/x-www-form-urlencoded"}
-    )
-    assert resp.ok, resp.text
-    logout_user()        # Delete Flask-Login's session cookie
-    del blueprint.token  # Delete OAuth token from storage
-    return redirect(url_for('welcome_user'))
+########FB_Login########
+fb_Oauth = make_facebook_blueprint(
+    client_id=fb_client_id,
+    client_secret=fb_client_secret,
+    # reprompt_consent=True,
+    # offline=True,
+    # scope=["profile", "email"],
+    # redirect_url='/welcome_fb'
+)
 
 
+app.register_blueprint(fb_Oauth, url_prefix="/login")
+@app.route("/login/fb")
+def fb_login():
+    if not facebook.authorized:
+        return redirect(url_for("facebook.login"))
+
+#####logout#####
+@app.route('/logout')
+@login_required
+def logout():
+    #delete google tocken
+    try:
+        token = google_Oauth.token["access_token"]
+        resp = google.post(
+            "https://accounts.google.com/o/oauth2/revoke",
+            params={"token": token},
+            headers={"Content-Type": "application/x-www-form-urlencoded"}
+        )
+        assert resp.ok, resp.text
+        logout_user()  # Delete Flask-Login's session cookie
+        del blueprint.token  # Delete OAuth token from storage
+    except:
+        pass
+    logout_user()
+    flash('You logged out!')
+    return redirect(url_for('home'))
 if __name__ == '__main__':
     app.run(debug=True)
+
+
---
+++ config.py
@@ -0,0 +1,4 @@
+google_client_id = "750796013081-i2sr7euqejkpk5e5bcppnbol6g1dpus3.apps.googleusercontent.com"
+google_client_secret = "TeCTIoCgSQWOx_iVsQZjV8pq"
+fb_client_id='533132573918405'
+fb_client_secret='6ceab60fadd06f4a08f29abb048c8a17'
