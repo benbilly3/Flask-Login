a2acf794e1dedf00b42ed9fa23cb0df2ce573ab4
Lun Tsai <benbilly3@Lunde-MacBook-Pro.local>
2019-12-11 18:05:11 +0800

    modify fb login

--- app.py
+++ app.py
@@ -182,7 +182,8 @@ def logout():
     logout_user()
     flash('You logged out!')
     return redirect(url_for('home'))
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='127.0.0.1', port=8000)
 
 
